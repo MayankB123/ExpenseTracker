bdc071488ab38e3028dc99caa974735d0fea4d96
MayankB123 <mayankbajpaix@gmail.com>
2024-08-05 16:48:05 +1000

    Changed pie chart color

--- public/js/expenses.js
+++ public/js/expenses.js
@@ -1,5 +1,5 @@
 const categories = ['Rent', 'Groceries', 'Utilities', 'Entertainment', 'Travel', 'Savings', 'Debt Repayment', 'Healthcare', 'Transportation', 'Miscellaneous']
-const backgroundColors = ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(255, 99, 71, 1)', 'rgba(148, 0, 211, 1)', 'rgba(255, 105, 180, 1)', 'rgba(0, 128, 128, 1)']
+const backgroundColors = ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(255, 99, 71, 1)', 'rgba(144, 238, 144, 1)', 'rgba(255, 105, 180, 1)', 'rgba(0, 128, 128, 1)']
 const grey = '#D3D3D3'
 let expenses;
 
